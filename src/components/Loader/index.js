213b3d1275c7430d60d0b4b11e74ee1645f6ff27
Arnab Mitra <mitraarnab.2013@gmail.com>
2023-04-09 12:36:39 +0530

    Added Feature

---
+++ src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Loader = props => (
+  <div>
+  <img
+    src={"https://loading.io/spinners/typing/lg.-text-entering-comment-loader.gif"}
+    alt="loading"
+   />
+  </div>
+)
+
+export default Loader;
